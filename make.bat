87d7a86bb8be62e2ae1058ef4be9f017e962271a
John Shahid & Ben Moss <pair+jshahid+bmoss@pivotal.io>
2015-03-11 11:52:15 -0400

    exit with an exit code

--- make.bat
+++ make.bat
@@ -16,47 +16,47 @@ REGEDIT.EXE  /S  "%~dp0\fix_visual_studio_building_msi.reg"
 
 :: install the binaries in %GOBIN%
 go get github.com/tools/godep
-pushd src\github.com\cloudfoundry-incubator\garden-windows || exit /b
-	godep restore || exit /b
-popd || exit /b
-go install github.com/coreos/etcd || exit /b
+pushd src\github.com\cloudfoundry-incubator\garden-windows || exit /b 1
+	godep restore || exit /b 1
+popd || exit /b 1
+go install github.com/coreos/etcd || exit /b 1
 
 :: Run the tests
 
-ginkgo -r -noColor src/github.com/cloudfoundry-incubator/garden-windows || exit /b
+ginkgo -r -noColor src/github.com/cloudfoundry-incubator/garden-windows || exit /b 1
 :: windows cmd doesn't like quoting arguments, use -skip=foo.bar instead of -skip='foo bar'
 :: we use the dot operator to match anything, -skip expects a regex
-ginkgo -skip=transition.to.running -r -noColor src/github.com/cloudfoundry-incubator/executor || exit /b
-ginkgo -skip=when.an.interrupt.signal.is.sent.to.the.representative^|should.not.exit,.but.keep.trying.to.maintain.presence.at.the.same.ID^|The.Rep.Evacuation.when.it.has.running.LRP.containers^|when.a.Ping.request.comes.in -noColor src/github.com/cloudfoundry-incubator/rep || exit /b
+ginkgo -skip=transition.to.running -r -noColor src/github.com/cloudfoundry-incubator/executor || exit /b 1
+ginkgo -skip=when.an.interrupt.signal.is.sent.to.the.representative^|should.not.exit,.but.keep.trying.to.maintain.presence.at.the.same.ID^|The.Rep.Evacuation.when.it.has.running.LRP.containers^|when.a.Ping.request.comes.in -noColor src/github.com/cloudfoundry-incubator/rep || exit /b 1
 
 :: Install the garden-windows, rep and executor in the MSI go-executables directory
 SET GOBIN=%CD%\src\github.com\cloudfoundry-incubator\containerizer\DiegoWindowsMSI\go-executables
-go install github.com/cloudfoundry-incubator/garden-windows
-go install github.com/cloudfoundry-incubator/executor/cmd/executor
-go install github.com/cloudfoundry-incubator/rep/cmd/rep
+go install github.com/cloudfoundry-incubator/garden-windows || exit /b 1
+go install github.com/cloudfoundry-incubator/executor/cmd/executor || exit /b 1
+go install github.com/cloudfoundry-incubator/rep/cmd/rep || exit /b 1
 
-pushd src\github.com\cloudfoundry-incubator\containerizer || exit /b
-	nuget restore || exit /b
-	devenv Containerizer\Containerizer.csproj /build "Release" || exit /b
-	devenv Containerizer.Tests\Containerizer.Tests.csproj /build "Release" || exit /b
-	packages\nspec.0.9.68\tools\NSpecRunner.exe Containerizer.Tests\bin\Debug\Containerizer.Tests.dll || exit /b
-	devenv DiegoWindowsMSI\DiegoWindowsMSI.vdproj /build "Release" || exit /b
-	copy DiegoWindowsMSI\Release\DiegoWindowsMSI.msi ..\..\..\..\output || exit /b
-popd || exit /b
+pushd src\github.com\cloudfoundry-incubator\containerizer || exit /b 1
+	nuget restore || exit /b 1
+	devenv Containerizer\Containerizer.csproj /build "Release" || exit /b 1
+	devenv Containerizer.Tests\Containerizer.Tests.csproj /build "Release" || exit /b 1
+	packages\nspec.0.9.68\tools\NSpecRunner.exe Containerizer.Tests\bin\Debug\Containerizer.Tests.dll || exit /b 1
+	devenv DiegoWindowsMSI\DiegoWindowsMSI.vdproj /build "Release" || exit /b 1
+	copy DiegoWindowsMSI\Release\DiegoWindowsMSI.msi ..\..\..\..\output || exit /b 1
+popd || exit /b 1
 
-pushd src\github.com\pivotal-cf-experimental\nora || exit /b
-	nuget restore || exit /b
-	msbuild Nora.sln || exit /b
-	packages\nspec.0.9.68\tools\NSpecRunner.exe Nora.Tests\bin\Debug\Nora.Tests.dll || exit /b
-popd || exit /b
+pushd src\github.com\pivotal-cf-experimental\nora || exit /b 1
+	nuget restore || exit /b 1
+	msbuild Nora.sln || exit /b 1
+	packages\nspec.0.9.68\tools\NSpecRunner.exe Nora.Tests\bin\Debug\Nora.Tests.dll || exit /b 1
+popd || exit /b 1
 
-pushd src\github.com\cloudfoundry-incubator\windows_app_lifecycle || exit /b
-	nuget restore || exit /b
-	devenv WindowsCircus.sln /build "Release" || exit /b
-	packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Debug\BuilderTests.dll || exit /b
-	packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Debug\LauncherTests.dll || exit /b
-	packages\nspec.0.9.68\tools\NSpecRunner.exe WebAppServer.Tests\bin\Debug\WebAppServer.Tests.dll || exit /b
-	bsdtar -czvf windows_app_lifecycle.tgz -C Builder\bin\Release . -C ..\..\..\Launcher\bin\Release . -C ..\..\..\Healthcheck\bin\Release . -C ..\..\..\WebAppServer\bin\Release .|| exit \b
-	copy windows_app_lifecycle.tgz ..\..\..\..\output || exit /b
-popd || exit /b
+pushd src\github.com\cloudfoundry-incubator\windows_app_lifecycle || exit /b 1
+	nuget restore || exit /b 1
+	devenv WindowsCircus.sln /build "Release" || exit /b 1
+	packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Debug\BuilderTests.dll || exit /b 1
+	packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Debug\LauncherTests.dll || exit /b 1
+	packages\nspec.0.9.68\tools\NSpecRunner.exe WebAppServer.Tests\bin\Debug\WebAppServer.Tests.dll || exit /b 1
+	bsdtar -czvf windows_app_lifecycle.tgz -C Builder\bin\Release . -C ..\..\..\Launcher\bin\Release . -C ..\..\..\Healthcheck\bin\Release . -C ..\..\..\WebAppServer\bin\Release .|| exit /b 1
+	copy windows_app_lifecycle.tgz ..\..\..\..\output || exit /b 1
+popd || exit /b 1
 
