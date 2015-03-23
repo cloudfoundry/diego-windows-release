f988a86a364f4f9b6d2b15af6a1b8e04dd034575
David Morhovich & Ben Moss <pair+dmorhovich+bmoss@pivotal.io>
2015-03-23 10:01:39 -0400

    Removes dism shell outs from FeatureManager project.

--- DiegoWindowsMSI/FeatureManager/FeatureManager.cs
+++ DiegoWindowsMSI/FeatureManager/FeatureManager.cs
@@ -50,24 +50,6 @@ namespace FeatureManager
             writePropertiesToFile(required);
         }
 
-        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
-        public override void Install(IDictionary stateSaver)
-        {
-            base.Install(stateSaver);
-
-            var dismPath = Environment.ExpandEnvironmentVariables(@"%WINDIR%\SysNative\dism.exe");
-
-            // enable windows features that the containerizer depends on in order to run the container host
-            var commands = new string[][] {
-		        new string[]{dismPath, "/online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart"},
-				new string[]{dismPath, "/online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart"},
-				new string[]{dismPath, "/online /Enable-Feature /FeatureName:Application-Server-WebServer-Support /FeatureName:AS-NET-Framework /All /NoRestart"},
-				new string[]{dismPath, "/online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart"},
-		    };
-
-            RunCommands(commands);
-        }
-
         private void writePropertiesToFile(List<string> keys)
         {
             var parameters = new Dictionary<string, string>();
@@ -79,36 +61,6 @@ namespace FeatureManager
             string jsonString = javaScriptSerializer.Serialize(parameters);
             var configFile = System.IO.Path.GetFullPath(System.IO.Path.Combine(Context.Parameters["assemblypath"], "..", "parameters.json"));
             System.IO.File.WriteAllText(configFile, jsonString);
-        }
-
-        private void RunCommands(string[][] commands)
-        {
-            foreach (var cmd in commands)
-            {
-                EventLog.WriteEntry(eventSource, "Enable feature " + cmd[1], EventLogEntryType.Information, 0);
-
-                var process = new Process
-                {
-                    StartInfo =
-                    {
-                        FileName = cmd[0],
-                        Arguments = cmd[1],
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true,
-                        UseShellExecute = false,
-                    }
-                };
-
-                process.Start();
-                process.WaitForExit();
-                if (process.ExitCode != 0)
-                {
-                    var message = "ERROR Enabling feature " + cmd[1] + "\r\n\r\n";
-                    message += process.StandardOutput.ReadToEnd() + "\r\n\r\n";
-                    message += process.StandardError.ReadToEnd();
-                    EventLog.WriteEntry(eventSource, message, EventLogEntryType.Error, 0);
-                }
-            }
         }
     }
 }
