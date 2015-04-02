b682c11c7566091435c6bde3bb63880d77f05177
Ben Moss & John Shahid <pair+jshahid+bmoss@pivotal.io>
2015-04-02 14:48:39 -0400

    Bump  submodules and change arguments to rep (from service)

--- DiegoWindowsMSI/RepService/RepService.cs
+++ DiegoWindowsMSI/RepService/RepService.cs
@@ -31,11 +31,26 @@ namespace RepService
 
             process = new Process
             {
+                /*
+                 *  -cellID="": the ID used by the rep to identify itself to external systems - must be specified
+ -communicationTimeout=10s: Timeout applied to all HTTP requests.
+ -consulCluster="": comma-separated list of consul server URLs (scheme://ip:port)
+ -evacuationPollingInterval=10s: the interval on which to scan the executor during evacuation
+ -evacuationTimeout=10m0s: Timeout to wait for evacuation to complete
+ -executorURL="http://127.0.0.1:1700": location of executor to represent
+ -heartbeatRetryInterval=5s: interval to wait before retrying a failed lock acquisition
+ -listenAddr="0.0.0.0:1800": host:port to serve auction and LRP stop requests on
+ -lockTTL=1m0s: TTL for service lock
+ -logLevel="info": log level: debug, info, error or fatal
+ -receptorTaskHandlerURL="http://127.0.0.1:1169": location of receptor task handler
+ -rootFSProvider=[]: List of RootFS providers
+                 */
                 StartInfo =
                 {
                     FileName = "rep.exe",
                     Arguments = " -etcdCluster=" + hash["ETCD_CLUSTER"] + " -debugAddr=0.0.0.0:17008 -preloadedRootFS=" + hash["STACK"] + ":/tmp/"+ hash["STACK"] + " -executorURL=http://127.0.0.1:1700 " +
-                                " -listenAddr=0.0.0.0:1800 -cellID=" + hash["MACHINE_NAME"] + " -zone=" + hash["ZONE"] + " -pollingInterval=30s -evacuationTimeout=180s",
+                                " -listenAddr=0.0.0.0:1800 -cellID=" + hash["MACHINE_NAME"] + " -zone=" + hash["ZONE"] + " -pollingInterval=30s -evacuationTimeout=180s" +
+                                " -consulCluster=http://127.0.0.1:8500 -receptorTaskHandlerURL=http://receptor.service.consul:1169",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false,
