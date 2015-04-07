11aec26ba403e62b16f098e995f7c774704bb4fc
John Shahid <jvshahid@gmail.com>
2015-04-07 15:02:13 -0400

    Disable Dnscache in setup.bat
    Dnscache will disable dns servers if they didn't respond/were down.
    This combined with the fact that the Consul agent keeps dying is causing
    name resolution to all cf components.
    
    There are three ways to solve this issue:
    
    1. Make sure Consul is stable
    2. Disable Dnscache
    3. Remove the state of the Dnscache on Consul restart
    
    Option 1 is not viable since we can't gurarntee that consul is up all
    the time especially during upgrades. Option 3 is also not viable since
    Dnscache state doesn't seem to be affected by `ipconfig /flushdns`, so
    we only have option 2.
    
    [#91881972]

--- setup.bat
+++ setup.bat
@@ -6,3 +6,10 @@ dism /online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart
 netsh interface ipv4 add dnsserver "Ethernet" address=127.0.0.1 index=1
 netsh interface ipv4 add dnsserver "Ethernet" address=8.8.4.4 index=2
 netsh interface ipv4 show dnsservers
+
+:: disable Dnscache setup [#91881972]
+:: Dnscache will disable dns servers if they didn't respond/were down.
+:: This combined with the fact that the Consul agent keeps dying is causing
+:: name resolution to all cf components.
+sc config Dnscache start= disabled
+sc stop Dnscache
