cloudfoundry/diego-windows-release

commit 91987e71ffa5bfa255bf06c85331ccaa65d91d7a

Clean up deploy_msi.rb
- Don't hardcode the machine ips
- JUMP_MACHINE_IP is now optional
- Make sure it works with bosh-lite

[#90817342]

scripts/deploy_msi.rb
@@ -5,14 +5,12 @@ require 'net/ssh/gateway'
 
 DEPLOYMENTS_RUNTIME = ENV['DEPLOYMENTS_RUNTIME'] or raise "Please set env var DEPLOYMENTS_RUNTIME"
 ADMIN_PASS = ENV['ADMIN_PASS'] or raise "Please set env var ADMIN_PASS"
-JUMP_MACHINE_IP = ENV['JUMP_MACHINE_IP'] or raise "Please set env var JUMP_MACHINE_IP"
+JUMP_MACHINE_IP = ENV['JUMP_MACHINE_IP']
 MACHINE_IP = ENV['MACHINE_IP'] or raise "Please set env var MACHINE_IP"
 CONSUL_IPS = ENV['CONSUL_IPS'] or raise "Please set env var CONSUL_IPS"
 ETCD_CLUSTER = ENV['ETCD_CLUSTER'] or raise "Please set env var ETCD_CLUSTER"
 CF_ETCD_CLUSTER = ENV['CF_ETCD_CLUSTER'] or raise "Please set env var CF_ETCD_CLUSTER"
 ZONE = ENV['ZONE'] or raise "Please set env var ZONE"
-ENV['GO_REVISION_DIEGO_WINDOWS_MSI'] or raise "Please set GO_REVISION_DIEGO_WINDOWS_MSI"
-expected_sha = ENV['GO_REVISION_DIEGO_WINDOWS_MSI'][0..6]
 
 options = {
   auth_methods: ["publickey"],
@@ -20,15 +18,35 @@ options = {
   keys: ["#{DEPLOYMENTS_RUNTIME}/keypair/id_rsa_bosh"]
 }
 
-msi_download_url = ARGV[0]
-unless msi_download_url && msi_download_url.match(/^http/)
-  ENV['GO_DEPENDENCY_LABEL_DIEGOMSI'] or raise  "Usage: $0 http://path/to/msi"
-  msi_download_url = "https://s3.amazonaws.com/diego-windows-msi/DiegoMSI/defaultStage/defaultJob/#{ENV['GO_DEPENDENCY_LABEL_DIEGOMSI']}.1/DiegoWindowsMSI-#{expected_sha}.msi"
-  puts msi_download_url
+# Figure out the sha of the msi being installed using the download url
+# or GO_REVISION_DIEGO_WINDOWS_MSI environment variable. The env.
+# variable is set by gocd since diego-windows-msi is one of the
+# Materials in the gocd job.
+def expected_sha
+  if sha_env = ENV['GO_REVISION_DIEGO_WINDOWS_MSI']
+    sha_env
+  elsif msi_download_url =~ /DiegoWindowsMSI-([0-9a-f]+).msi$/
+    $1
+  else
+    raise "Pass either a download url or set GO_REVISION_DIEGO_WINDOWS_MSI"
+  end
 end
+
+
+# Return the msi download url, using either the first argument or the
+# GO_DEPENDENCY_LABEL_DIEGOMSI environment variable (which is the gocd
+# job id of the last DiegoWindowsMSI build)
+def msi_download_url
+  url = ARGV[0]
+  # return the argument if it was provided and is valid
+  return url if url && url =~ /^http/
+
+  job_id = ENV['GO_DEPENDENCY_LABEL_DIEGOMSI'] or raise  "Usage: $0 http://path/to/msi"
+  "https://s3.amazonaws.com/diego-windows-msi/DiegoMSI/defaultStage/defaultJob/#{job_id}.1/DiegoWindowsMSI-#{expected_sha}.msi"
+end
+
 msi_location="c:\\diego.msi"
-gateway = Net::SSH::Gateway.new('54.84.34.184', 'ec2-user', options)
-gateway.ssh("10.10.12.134", "ci", options) do |ssh|
+block = ->(ssh) do
   hostname = ssh.exec!("hostname").chomp
   puts "Hostname: #{hostname}"
 
@@ -51,4 +69,11 @@ gateway.ssh("10.10.12.134", "ci", options) do |ssh|
     exit(1)
   end
   puts "Installation succeeded, #{expected_sha} == #{actual_sha}"
+end
+
+if JUMP_MACHINE_IP
+  gateway = Net::SSH::Gateway.new(JUMP_MACHINE_IP, 'ec2-user', options)
+  gateway.ssh(MACHINE_IP, "ci", options, &block)
+else
+  Net::SSH.start(MACHINE_IP, "ci", options, &block)
 end

scripts/deploy_msi.sh
@@ -4,4 +4,4 @@ chruby 2.1.2
 gem install bundler --no-rdoc --no-ri
 export BUNDLE_GEMFILE=$(dirname $0)/Gemfile
 bundle
-bundle exec $(dirname $0)/deploy_msi.rb
+bundle exec $(dirname $0)/deploy_msi.rb "$@"

commit cf0ebf7ed790cbdfff3ac9c6e4ee49d1a3ffc9bd

Fix a bug accidentally introduced in 91987e7
[#91849586]

scripts/deploy_msi.rb
@@ -24,7 +24,7 @@ options = {
 # Materials in the gocd job.
 def expected_sha
   if sha_env = ENV['GO_REVISION_DIEGO_WINDOWS_MSI']
-    sha_env
+    sha_env[0..6]
   elsif msi_download_url =~ /DiegoWindowsMSI-([0-9a-f]+).msi$/
     $1
   else
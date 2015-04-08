cf0ebf7ed790cbdfff3ac9c6e4ee49d1a3ffc9bd
Ben Moss and John Shahid <pair+bmoss+jvshahid@pivotal.io>
2015-04-08 12:27:12 -0400

    Fix a bug accidentally introduced in 91987e7
    [#91849586]

--- scripts/deploy_msi.rb
+++ scripts/deploy_msi.rb
@@ -24,7 +24,7 @@ options = {
 # Materials in the gocd job.
 def expected_sha
   if sha_env = ENV['GO_REVISION_DIEGO_WINDOWS_MSI']
-    sha_env
+    sha_env[0..6]
   elsif msi_download_url =~ /DiegoWindowsMSI-([0-9a-f]+).msi$/
     $1
   else
