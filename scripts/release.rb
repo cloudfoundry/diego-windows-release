a6a17be91f6444feb900409da2bf5211732c5cd4
David Morhovich and John Shahid <pair+dmorhovich+jshahid@pivotal.io>
2015-04-14 17:10:06 -0400

    Include cf/diego release sha in the release assets
    [#92261222]

--- scripts/release.rb
+++ scripts/release.rb
@@ -37,6 +37,18 @@ def s3_url
   "https://s3.amazonaws.com/diego-windows-msi/output/DiegoWindowsMSI-#{short_sha}.msi"
 end
 
+def bosh_target
+  env_var 'BOSH_TARGET'
+end
+
+def bosh_user
+  env_var 'BOSH_USER'
+end
+
+def bosh_password
+  env_var 'BOSH_PASSWORD'
+end
+
 def create_github_tag
   github.create_tag repo,
                     release,
@@ -63,6 +75,15 @@ def download_msi
   temp_file
 end
 
+def grab_cf_diego_release_sha
+  puts "Grabbing cf/diego release shas from #{bosh_target}"
+  temp_file = "/tmp/cf_diego_release_sha.txt"
+  File.open(temp_file, "wb+") do |f|
+    f.write `bosh -t #{bosh_target} -u #{bosh_user} -p #{bosh_password} releases`
+  end
+  temp_file
+end
+
 def upload_release_assets filepath, release
   filename = File.basename filepath
   github.upload_asset release[:url],
@@ -82,3 +103,8 @@ puts "Downloaded msi from s3"
 puts "Uploading msi to github release"
 upload_release_assets file, res
 puts "Uploaded msi to github release"
+
+puts "Grabbing cf/diego release sha"
+file = grab_cf_diego_release_sha
+upload_release_assets file, res
+puts "Grabbed and uploaded cf/diego release sha"
