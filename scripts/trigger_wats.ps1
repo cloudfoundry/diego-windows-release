9afbc5382c5447a1479cfd22a6aeef9b29fcc469
John Shahid <jvshahid@gmail.com>
2015-04-14 11:36:05 -0400

    Propagate the git sha from appveyor to gocd
    [#91184246]

--- scripts/trigger_wats.ps1
+++ scripts/trigger_wats.ps1
@@ -1,5 +1,6 @@
 $username=$env:USERNAME
 $password=$env:PASSWORD
 $uri=$env:URI
+$commit=$env:APPVEYOR_REPO_COMMIT
 $base64AuthInfo=[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}"-f$username,$password)))
-Invoke-RestMethod -Headers @{Authorization=("Basic {0}"-f$base64AuthInfo)} -Method Post -Uri $uri
+Invoke-RestMethod -Headers @{Authorization=("Basic {0}"-f$base64AuthInfo)} -Method Post -Uri $uri -Body @{"materials[diego-windows-msi]" =$commit}
