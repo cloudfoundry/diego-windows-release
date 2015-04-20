3587e6b611733b8894b4a992a846c01ba56a8fa2
John_Shahid <jshahid@pivotal.io>
2015-04-20 14:40:38 -0400

    Enhance the deploy_to_bosh_lite
    * Figure out which commit we want to deploy from the most recent tag
    * Include the ADMIN_PASS, since this is a private repo
    * Set ZONE to `z1' by default
    
    Signed-off-by: John_Shahid <jshahid@pivotal.io>

--- scripts/deploy_to_bosh_lite.sh
+++ scripts/deploy_to_bosh_lite.sh
@@ -2,17 +2,21 @@
 
 cd `dirname $0`
 
-if [ "x$ADMIN_PASS" == "x" ]; then
-    echo "Please set ADMIN_PASS env variable"
-    exit 1
-fi
-
 #JUMP_MACHINE_IP
-export ADMIN_PASS
+
+# TODO: JS & DM screwed up the v0.400 tag and it can't be deleted
+# Until it is deleted using the github api leave the `grep -v` below
+tag=`git tag --sort="v:refname" | grep -v v0.400 | tail -n1`
+commit=`git show -s --oneline ${tag} | awk '{print $1}'`
+export GO_REVISION_DIEGO_WINDOWS_MSI=${commit}
+export ADMIN_PASS=joE3Jj5Fex!
+export ZONE=z1
 export MACHINE_IP=172.16.234.139
 export CONSUL_IPS=10.244.0.54
 export ETCD_CLUSTER=http://10.244.16.2:4001
 export CF_ETCD_CLUSTER=http://10.244.0.42:4001
 export DEPLOYMENTS_RUNTIME=~/workspace/deployments-runtime/ketchup
 
-./deploy_msi.sh "$@"
+echo "Installing version: ${tag}:${commit}"
+
+./run_ruby.sh ./deploy_msi.rb "$@"
