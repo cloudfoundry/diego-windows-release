d771181265973ab450f986c851f13131fba19711
Ben_Moss <bmoss@pivotal.io>
2015-04-27 15:51:47 -0400

    Namespace environment variables
    Signed-off-by: Dave_Goddard <dgoddard@pivotal.io>

--- scripts/trigger_wats.ps1
+++ scripts/trigger_wats.ps1
@@ -1,5 +1,5 @@
-$username=$env:USERNAME
-$password=$env:PASSWORD
+$username=$env:GOCD_USERNAME
+$password=$env:GOCD_PASSWORD
 $uri=$env:URI
 $commit=$env:APPVEYOR_REPO_COMMIT
 $base64AuthInfo=[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}"-f$username,$password)))
