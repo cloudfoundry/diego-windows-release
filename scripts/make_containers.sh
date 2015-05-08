746aed275d797bc61726da20178fe463b4bc675c
Ben Moss <ben@mossity.com>
2015-05-08 12:24:11 -0400

    Update make_containers to lower number of containers
    We only need around 115 containers to reach the limit that Windows service processes have on subprocesses.

--- scripts/make_containers.sh
+++ scripts/make_containers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in {1..200}; do
+for i in {1..115}; do
     curl -H 'content-type: application/json' --data @- http://localhost:1788/api/containers <<EOF
 {
   "Handle": "anotherhandle$i"
@@ -8,6 +8,6 @@ for i in {1..200}; do
 EOF
 done
 
-for i in {1..200}; do
+for i in {1..115}; do
     curl -X DELETE http://localhost:1788/api/containers/anotherhandle$i
 done
