a0c290b2e4362c6e32eda21de82abb9433121d7a
Dave_Goddard <dgoddard@pivotal.io>
2015-05-11 12:16:15 -0400

    Fix New-NetFirewallRule ; script is now idempotent

--- scripts/setup_firewall.ps1
+++ scripts/setup_firewall.ps1
@@ -4,10 +4,11 @@ $adminsSid = $admins.Translate([System.Security.Principal.SecurityIdentifier])
 $currentUser = New-Object System.Security.Principal.NTAccount($env:username)
 $currentUserSid = $currentUser.Translate([System.Security.Principal.SecurityIdentifier])
 
-New-NetFirewallRule -Name "AllowAdmins" -DisplayName "Allow admins" `
+Remove-NetFirewallRule -Name "CFAllowAdmins" -Erroraction 'silentlycontinue'
+New-NetFirewallRule -Name "CFAllowAdmins" -DisplayName "Allow admins" `
   -Description "Allow admin users" -RemotePort Any `
   -LocalPort Any -LocalAddress Any -RemoteAddress Any `
   -Enabled True -Profile Any -Action Allow -Direction Outbound `
   -LocalUser "D:(A;;CC;;;$adminsSid) (A;;CC;;;$currentUserSid)"
 
-set-netfirewallprofile -all -DefaultInboundAction Allow -DefaultOutboundAction Block
+set-netfirewallprofile -all -DefaultInboundAction Allow -DefaultOutboundAction Block -Enabled True
