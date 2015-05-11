c1ae09e935db3c77a9ddf730db8cf9e680c28486
Ben_Moss <bmoss@pivotal.io>
2015-05-11 09:25:28 -0400

    Disable firewalls in setup.bat

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -17,3 +17,5 @@ sc stop Dnscache
 :: increases desktop heap limit, see http://stackoverflow.com/questions/17472389/how-to-increase-the-maximum-number-of-child-processes-that-can-be-spawned-by-a-w/17472390#17472390
 REGEDIT.EXE  /S  "%~dp0\increase_desktop_heap_for_services.reg" || exit /b 1
 
+:: disable all windows firewall profiles
+ NetSh Advfirewall set allprofiles state off
