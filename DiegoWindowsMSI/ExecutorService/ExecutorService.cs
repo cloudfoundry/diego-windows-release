0348e2b9667b38685ecc83e6f3c6346a03a67fd4
Dave_Goddard <dgoddard@pivotal.io>
2015-05-15 16:24:35 -0400

    Set CPU HardCap range in executor to 1-10,000
    [#94528112]
    
    Signed-off-by: Ben_Moss <bmoss@pivotal.io>

--- DiegoWindowsMSI/ExecutorService/ExecutorService.cs
+++ DiegoWindowsMSI/ExecutorService/ExecutorService.cs
@@ -33,7 +33,7 @@ namespace ExecutorService
                 {
                     FileName = "executor.exe",
                     Arguments = " -listenAddr=0.0.0.0:1700 -skipCertVerify=false -debugAddr=0.0.0.0:17004 -gardenNetwork=tcp -gardenAddr=localhost:9241 " +
-                                " -memoryMB=auto -diskMB=auto -containerMaxCpuShares=9 -tempDir=/c/tmp/executor/tmp -cachePath=/c/tmp/executor/cache -maxCacheSizeInBytes=10000000000 " +
+                                " -memoryMB=auto -diskMB=auto -containerMaxCpuShares=10000 -tempDir=/c/tmp/executor/tmp -cachePath=/c/tmp/executor/cache -maxCacheSizeInBytes=10000000000 " +
                                 " -allowPrivileged=false -exportNetworkEnvVars=false -drainTimeout=900s -healthyMonitoringInterval=30s -unhealthyMonitoringInterval=0.5s",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
