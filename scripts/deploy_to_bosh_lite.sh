06563b945be3142753627ea91a6690448bd9294f
John_Shahid <jshahid@pivotal.io>
2015-05-19 15:38:46 -0400

    Remove a comment that isn't relevant and include the secret
    Signed-off-by: Dave_Goddard <dgoddard@pivotal.io>

--- scripts/deploy_to_bosh_lite.sh
+++ scripts/deploy_to_bosh_lite.sh
@@ -4,8 +4,6 @@ cd `dirname $0`
 
 #JUMP_MACHINE_IP
 
-# TODO: JS & DM screwed up the v0.400 tag and it can't be deleted
-# Until it is deleted using the github api leave the `grep -v` below
 tag=`git tag --sort="v:refname" | grep -v v0.400 | tail -n1`
 commit=`git show -s --oneline ${tag} | awk '{print $1}'`
 export GO_REVISION_DIEGO_WINDOWS_MSI=${commit}
@@ -16,6 +14,7 @@ export CONSUL_IPS=10.244.0.54
 export ETCD_CLUSTER=http://10.244.16.2:4001
 export CF_ETCD_CLUSTER=http://10.244.0.42:4001
 export DEPLOYMENTS_RUNTIME=~/workspace/deployments-runtime/ketchup
+export LOGGREGATOR_SHARED_SECRET=loggregator-secret
 
 echo "Installing version: ${tag}:${commit}"
 
