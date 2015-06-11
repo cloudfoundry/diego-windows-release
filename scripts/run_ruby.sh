d2b9275e34ff143cc816bd952a68b31f464f58dc
John_Shahid <jshahid@pivotal.io>
2015-06-11 11:03:27 -0400

    Set GEM_PATH and GEM_HOME in run_ruby.sh
    [#93057508]
    
    Signed-off-by: Ben_Moss <bmoss@pivotal.io>

--- scripts/run_ruby.sh
+++ scripts/run_ruby.sh
@@ -2,6 +2,8 @@
 
 chruby 2.1.2
 gem install bundler --no-rdoc --no-ri
+export GEM_HOME=~/.gems
+export GEM_PATH=$GEM_HOME:$GEM_PATH
 export BUNDLE_GEMFILE=$(dirname $0)/Gemfile
 bundle install
 ruby_script=$1
