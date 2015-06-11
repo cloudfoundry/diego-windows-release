2fef77f21028bf21dae43279ef6aa6bd1d84709f
John_Shahid <jshahid@pivotal.io>
2015-06-11 11:16:57 -0400

    Move the export GEM_HOME and GEM_HOME up
    Signed-off-by: Ben_Moss <bmoss@pivotal.io>

--- scripts/run_ruby.sh
+++ scripts/run_ruby.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 chruby 2.1.2
-gem install bundler --no-rdoc --no-ri
 export GEM_HOME=~/.gems
 export GEM_PATH=$GEM_HOME:$GEM_PATH
 export BUNDLE_GEMFILE=$(dirname $0)/Gemfile
+gem install bundler --no-rdoc --no-ri
 bundle install
 ruby_script=$1
 shift
