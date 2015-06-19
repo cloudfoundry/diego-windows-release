7e2012c8b25b2a252e8499c24115ce92dda7967b
Ben_Moss <bmoss@pivotal.io>
2015-06-19 17:13:18 -0400

    Stop triggering builds on gocd

--- scripts/trigger_wats.ps1
+++
@@ -1,6 +0,0 @@
-$username=$env:GOCD_USERNAME
-$password=$env:GOCD_PASSWORD
-$uri=$env:URI
-$commit=$env:APPVEYOR_REPO_COMMIT
-$base64AuthInfo=[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}"-f$username,$password)))
-Invoke-RestMethod -Headers @{Authorization=("Basic {0}"-f$base64AuthInfo)} -Method Post -Uri $uri -Body @{"materials[diego-windows-msi]" =$commit}
