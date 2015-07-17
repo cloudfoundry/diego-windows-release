8341bd0f77f50b6b30863ed4a923bec40d9f5091
Dave Goddard <dgoddard@pivotal.io>
2015-07-17 14:32:27 -0400

    Update Github release with cf/diego sha if release already exists
    Signed-off-by: David Morhovich <dmorhovich@pivotal.io>

--- scripts/release.rb
+++ scripts/release.rb
@@ -18,10 +18,6 @@ def msi_file
   Dir::glob('./msi-file/DiegoWindowsMSI-*-*.msi').first
 end
 
-def release
-  "v#{File.read('./msi-file/version').chomp}"
-end
-
 def github
   @github ||= Octokit::Client.new access_token: token
 end
@@ -45,16 +41,20 @@ def create_github_tag
                     "pivotal-netgarden-eng@pivotal.io", # tagger email isn't being used by the api
                     Time.now.utc.iso8601
 
+  github.create_release(repo,
+                        release,
+                        name: release,
+                        body: cf_diego_release_text(release))
+end
+
+def cf_diego_release_text(release)
   diego_release = revision("diego-release") rescue "UNKNOWN"
   cf_release = revision("cf-release") rescue "UNKNOWN"
   release_body = <<-BODY
 cloudfoundry-incubator/diego-release@#{diego_release}
 cloudfoundry/cf-release@#{cf_release}
 BODY
-  github.create_release(repo,
-                        release,
-                        name: release,
-                        body: release_body)
+  release_body
 end
 
 def content_type filename
@@ -73,27 +73,32 @@ def upload_release_assets(filepath, release, filename=nil)
                       name: filename
 end
 
-def release_already_created?(release)
-  github.releases(repo).any? {|r| r.tag_name == release}
+def get_release_resource(release)
+  github.releases(repo).select { |r| r.tag_name == release}.first
 end
 
-if release_already_created?(release)
-  puts "Release #{release} already created, skipping"
-  exit(0)
+release = "v#{File.read('./msi-file/version').chomp}"
+
+release_resource = get_release_resource(release)
+if release_resource then
+  puts "Update Existing Resource"
+  body = release_resource.body
+  body += "\n\n-------------\n" + cf_diego_release_text(release)
+  github.update_release(release.url, { body: body })
+else
+  puts "Creating github release"
+  res = create_github_tag
+  puts "Created github release"
+
+  puts "Uploading msi to github release"
+  upload_release_assets msi_file, res, "DiegoWindowsMSI.msi"
+  puts "Uploaded msi to github release"
+
+  puts "Uploading installation instructions to github release"
+  upload_release_assets "diego-windows-msi/docs/INSTALL.md", res
+  puts "Uploaded installation instructions to github release"
+
+  puts "Uploading setup script to github release"
+  upload_release_assets "diego-windows-msi/scripts/setup.ps1", res
+  puts "Uploaded setup script to github release"
 end
-
-puts "Creating github release"
-res = create_github_tag
-puts "Created github release"
-
-puts "Uploading msi to github release"
-upload_release_assets msi_file, res, "DiegoWindowsMSI.msi"
-puts "Uploaded msi to github release"
-
-puts "Uploading installation instructions to github release"
-upload_release_assets "diego-windows-msi/docs/INSTALL.md", res
-puts "Uploaded installation instructions to github release"
-
-puts "Uploading setup script to github release"
-upload_release_assets "diego-windows-msi/scripts/setup.ps1", res
-puts "Uploaded setup script to github release"
