9b53eeaad8944af11cecac1232d46739fd0722a3
John Shahid <jshahid@pivotal.io>
2015-09-30 17:10:02 -0400

    Sets the MSI version to appveyor build number
    [#104542416]

---
+++ scripts/replace-version.ps1
@@ -0,0 +1,13 @@
+$productcode = [guid]::NewGuid().ToString().ToUpper()
+$packagecode = [guid]::NewGuid().ToString().ToUpper()
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+push-location $dir
+  $oldversion = """ProductVersion"" = ""8:1.0.0"""
+  $version = """ProductVersion"" = ""8:$env:APPVEYOR_BUILD_VERSION"""
+  $oldproductcode = """ProductCode"" = ""8:{48F8F0E2-6555-4500-8868-C9F8FD9394F2}"""
+  $productcode = """ProductCode"" = ""8:{$productcode}"""
+  $oldpackagecode = """PackageCode"" = ""8:{E5C8395D-FB02-4615-9865-0BBECC3076D6}"""
+  $packagecode = """PackageCode"" = ""8:{$packagecode}"""
+  (get-content ..\DiegoWindowsRelease\DiegoWindowsMSI\DiegoWindowsMSI.vdproj).replace("$oldversion","$version").replace("$oldproductcode", "$productcode").replace("$oldpackagecode", "$packagecode") | set-content ..\DiegoWindowsRelease\DiegoWindowsMSI\DiegoWindowsMSI.vdproj
+pop-location
