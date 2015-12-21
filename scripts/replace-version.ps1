f4149ea9b7ca977a84b55c4528c4bcd32bdc2850
Micah Young <myoung@pivotal.io>
2015-12-21 17:00:10 -0500

    Set MSI major version to 0
    Minor versions have a limit of 256.
    
    Signed-off-by: Matthew Horan <mhoran@pivotal.io>

--- scripts/replace-version.ps1
+++ scripts/replace-version.ps1
@@ -4,7 +4,7 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 push-location $dir
   $oldversion = """ProductVersion"" = ""8:1.0.0"""
-  $version = """ProductVersion"" = ""8:$env:APPVEYOR_BUILD_VERSION"""
+  $version = """ProductVersion"" = ""8:0.$env:APPVEYOR_BUILD_VERSION"""
   $oldproductcode = """ProductCode"" = ""8:{48F8F0E2-6555-4500-8868-C9F8FD9394F2}"""
   $productcode = """ProductCode"" = ""8:{$productcode}"""
   $oldpackagecode = """PackageCode"" = ""8:{E5C8395D-FB02-4615-9865-0BBECC3076D6}"""
