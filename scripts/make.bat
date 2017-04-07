09338376b78d7a975df34c766578f70b368d03d4
Charlie Vieth <cviethjr@pivotal.io>
2017-04-07 12:46:20 -0400

    remove skip comment
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- scripts/make.bat
+++ scripts/make.bat
@@ -47,8 +47,6 @@ popd
 
 pushd diego-release || exit /b 1
   SET GOPATH=%CD%
-  :: windows cmd doesn't like quoting arguments, use -skip=foo.bar
-  :: instead of -skip='foo bar'
   ginkgo -r -noColor src/code.cloudfoundry.org/executor || exit /b 1
   ginkgo -noColor src/code.cloudfoundry.org/rep || exit /b 1
 
